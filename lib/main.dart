6129574da3ae3959b95924574f6decf2e645e58b
Sholokhov Maxim <scholoxow.maxim@gmail.com>
2026-02-23 22:09:54 +0300

    feat: setup clean architecture structure and base theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,5 @@
-// lib/main.dart
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'providers/theme_provider.dart';
-import 'providers/player_provider.dart';
-import 'providers/favorites_provider.dart';
-import 'screens/splash_screen.dart';
-import 'theme/app_theme.dart';
+import 'core/theme/app_theme.dart';
 
 void main() {
   runApp(const RadioMDApp());
@@ -16,23 +10,14 @@ class RadioMDApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => ThemeProvider()),
-        ChangeNotifierProvider(create: (_) => PlayerProvider()),
-        ChangeNotifierProvider(create: (_) => FavoritesProvider()),
-      ],
-      child: Consumer<ThemeProvider>(
-        builder: (context, themeProvider, child) {
-          return MaterialApp(
-            title: 'RadioMD',
-            debugShowCheckedModeBanner: false,
-            theme: AppTheme.lightTheme,
-            darkTheme: AppTheme.darkTheme,
-            themeMode: themeProvider.themeMode,
-            home: const SplashScreen(),
-          );
-        },
+    return MaterialApp(
+      title: 'RadioMD',
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.lightTheme,
+      home: const Scaffold(
+        body: Center(
+          child: Text('RadioMD'),
+        ),
       ),
     );
   }
