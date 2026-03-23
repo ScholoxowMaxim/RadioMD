06ca999c291b98c8b545010251214a66e46f967d
ScholoxowMaxim <scholoxow.maxim@gmail.com>
2026-03-23 18:43:56 +0300

    v0.0.1 добавленно работающее радио

--- lib/core/services/player_service.dart
+++ lib/core/services/player_service.dart
@@ -1,24 +1,50 @@
 import 'package:just_audio/just_audio.dart';
+import 'dart:async';
+import '../../features/home/domain/station.dart';
 
 class PlayerService {
+  static final PlayerService _instance = PlayerService._internal();
+  factory PlayerService() => _instance;
+  PlayerService._internal();
+
   final AudioPlayer _player = AudioPlayer();
 
-  Future<void> play(String url) async {
-    await _player.setUrl(url);
-    _player.play();
+  final StreamController<Station?> _stationController =
+      StreamController<Station?>.broadcast();
+
+  Station? _currentStation;
+
+  Station? get currentStation => _currentStation;
+
+  Stream<Station?> get stationStream => _stationController.stream;
+
+  bool get isPlaying => _player.playing;
+
+  Stream<PlayerState> get playerStateStream => _player.playerStateStream;
+
+  Future<void> play(Station station) async {
+    _currentStation = station;
+    _stationController.add(station);
+    await _player.setUrl(station.streamUrl);
+    await _player.play();
   }
 
-  Future<void> pause() async {
-    await _player.pause();
+  Future<void> toggle() async {
+    if (_player.playing) {
+      await _player.pause();
+    } else if (_currentStation != null) {
+      await _player.play();
+    }
   }
 
   Future<void> stop() async {
     await _player.stop();
+    _currentStation = null;
+    _stationController.add(null);
   }
 
-  Stream<PlayerState> get playerStateStream => _player.playerStateStream;
-
   void dispose() {
+    _stationController.close();
     _player.dispose();
   }
 }
